6d876262e3fed236959ed64bffccfb69f123b4c0
Evgenya <subbotinjon01@mail.ru>
2019-11-20 21:57:52 +0300

    Create project

---
+++ app/src/main/java/com/game/story_about_one_hero/MainActivity.java
@@ -0,0 +1,14 @@
+package com.game.story_about_one_hero;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
